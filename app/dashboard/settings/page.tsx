1f034f7cc61f6e0e61bb2e55c3711a04d18e4494
bspraveen17 <billasujith77@gmail.com>
2024-02-14 01:39:18 +0530

    Setup settings page and add dydynamic theme changing functionality

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,127 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import prisma from "@/lib/db";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Input } from "@/components/ui/input";
+import { SubmitButton } from "@/components/SubmitButton";
+import { revalidatePath } from "next/cache";
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      name: true,
+      email: true,
+      colorScheme: true,
+    },
+  });
+  return data;
+}
+
+export default async function SettingsPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
+
+  async function postData(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name") as string;
+    const colorScheme = formData.get("color") as string;
+    await prisma.user.update({
+      where: {
+        id: user?.id,
+      },
+      data: {
+        name: name ?? undefined,
+        colorScheme: colorScheme ?? undefined,
+      },
+    });
+    revalidatePath("/", "layout");
+  }
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl">Settings</h1>
+          <p className="text-lg text-muted-foreground">Your profile settings</p>
+        </div>
+      </div>
+      <Card>
+        <form action={postData}>
+          <CardHeader>
+            <CardTitle>General Data</CardTitle>
+            <CardDescription>
+              Please provide your general information
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-1">
+              <Label>Your Name</Label>
+              <Input
+                name="name"
+                type="name"
+                id="name"
+                placeholder="Enter your name"
+                defaultValue={data?.name ?? ""}
+              />
+            </div>
+            <div className="space-y-1">
+              <Label>Your Email</Label>
+              <Input
+                name="email"
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                disabled
+                defaultValue={data?.email ?? ""}
+              />
+            </div>
+            <div className="space-y-1">
+              <Label>Select Color</Label>
+              <Select name="color" defaultValue={data?.colorScheme}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a color" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Color</SelectLabel>
+                    <SelectItem value="theme-green">Green</SelectItem>
+                    <SelectItem value="theme-blue">Blue</SelectItem>
+                    <SelectItem value="theme-violet">Violet</SelectItem>
+                    <SelectItem value="theme-yellow">Yellow</SelectItem>
+                    <SelectItem value="theme-orange">Orange</SelectItem>
+                    <SelectItem value="theme-red">Red</SelectItem>
+                    <SelectItem value="theme-rose">Rose</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <SubmitButton />
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+}
---
+++ components/SubmitButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+          Please Wait...
+        </Button>
+      ) : (
+        <Button type="submit" className="w-fit">
+          Save
+        </Button>
+      )}
+    </>
+  );
+}
